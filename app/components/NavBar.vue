d1605d9c4aa50e07dd12398bba251d27bce22aa6
Mikołaj Mądrzyk <matvete@hotmail.com>
2026-04-05 18:58:44 +0200

    feat: created navbar template

---
+++ app/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="~/assets/logo.svg" alt="Zerya logo" width="108px" height="52px">
+  <div class="main_links">
+    <div>Home</div>
+    <div>About Us</div>
+    <div>Services</div>
+    <div>Case Studies</div>
+    <div>Career</div>
+  </div>
+  <div class="contact_button">
+    Contact Us
+  </div>
+</template>
